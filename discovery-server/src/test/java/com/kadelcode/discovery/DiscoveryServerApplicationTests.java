c499802991f2341d8d1e055f93f754d623433d19
ayodele <kadelcode@gmail.com>
2026-06-30 13:20:32 +0100

    feat(discovery): enable Eureka server

---
+++ discovery-server/src/test/java/com/kadelcode/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kadelcode.discovery;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    @DisplayName("Should load the Sprint application context")
+    void contextLoads() {
+    }
+
+}
