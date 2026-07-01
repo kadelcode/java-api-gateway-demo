e369b1b17c1e28169f8f1e38236a014f894978f2
ayodele <kadelcode@gmail.com>
2026-07-01 02:05:45 +0100

    feat(user): add user model, controller and service with tests (#8)

--- user-service/src/main/java/com/kadelcode/user/UserServiceApplication.java
+++ user-service/src/main/java/com/kadelcode/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.kadelcode.userservice;
+package com.kadelcode.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
